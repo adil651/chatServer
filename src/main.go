943d6b299a1cafa336a7cb5bb37d4cafa28a98c0
Adil Iqbal <adi.iqbal65@gmail.com>
2019-05-25 01:22:36 +0500

    Back End: Upgraded HTTP connection to Web Socket connection

--- src/main.go
+++ src/main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{}
 
 func main() {
 
@@ -11,11 +18,26 @@ func main() {
 	fs := http.FileServer(http.Dir("../public"))
 	http.Handle("/", fs)
 
-	log.Println("http server started on :8000")
 	// Start the HTTP server
+	log.Println("http server started on :8000")
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
 }
