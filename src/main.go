7197c06927456cd4d913ea6971510affadcf9dc5
Adil Iqbal <adi.iqbal65@gmail.com>
2019-05-25 01:07:51 +0500

    BackEnd: Developed basic file server over HTTP Server to deliver static files

--- src/main.go
+++ src/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 
+	// Create a simple file server
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+
 	log.Println("http server started on :8000")
 	// Start the HTTP server
 	err := http.ListenAndServe(":8000", nil)
